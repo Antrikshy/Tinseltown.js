99a510c05919df98e4b8ec8d44d2b5f826ec41a0
Antriksh Yadav <antrikshy@gmail.com>
2016-08-14 23:09:42 +0800

    Got carried away designing most of the site

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
-    gulp.src('./styles/*.scss')
+    gulp.src('./lib/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 2 versions', '> 1%', 'Firefox ESR']}))
         .pipe(gulp.dest('./lib/'));
 });
 
-gulp.watch('./styles/*.scss', function() {
+gulp.watch('./lib/*.scss', function() {
     gulp.start('default');
 });
