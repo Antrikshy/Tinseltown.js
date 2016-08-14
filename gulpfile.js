402216b7b6b6d7392ef3f69aecf3e0af2000c9d3
Antriksh Yadav <antrikshy@gmail.com>
2016-08-14 15:53:49 +0800

    Set up basic site, Bulma

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass-it-up', function() {
+gulp.task('default', function() {
     gulp.src('./styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 2 versions', '> 1%', 'Firefox ESR']}))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('./lib/'));
 });
 
-gulp.task('default', function() {
-    gulp.start('sass-it-up');
-});
-
-gulp.watch('./src/*.scss', function() {
-    gulp.start('sass-it-up');
+gulp.watch('./styles/*.scss', function() {
+    gulp.start('default');
 });
